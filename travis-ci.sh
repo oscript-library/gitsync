9c058d68fd3d4abc8ddbec9c1488c214bed5b04a
Хорев Алексей Андреевич <khorevaa@art-vizing.ru>
2018-01-08 16:50:33 +0300

    Корректировка пути скритпа

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,9 +1,12 @@
 
-git config --global user.name "ci"
-git config --global user.email "ci@me"
+
+GITNAME="${GIT_NAME:-"ci"}"
+GITEMAIL="${GIT_EMAIL:-"ci@me"}"
+
+git config --global user.name $GITNAME
+git config --global user.email $GITEMAIL
 
 opm install; 
 opm install 1testrunner; 
 opm install 1bdd; 
 opm test; 
-
