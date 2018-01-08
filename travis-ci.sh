7a6489c4caad248dbed2b8cf7780f9b0f70e8c6b
khorevaa <khorevaa@gmail.com>
2018-01-08 17:54:57 +0300

    Добавил установку wine

--- travis-ci.sh
+++ travis-ci.sh
@@ -6,6 +6,11 @@ GITEMAIL="${GIT_EMAIL:-"ci@me"}"
 git config --global user.name $GITNAME
 git config --global user.email $GITEMAIL
 
+if [ $TRAVIS_OS_NAME == "linux" ]; then 
+    export CXX="g++-4.9" CC="gcc-4.9" DISPLAY=:99.0;
+    linux32 apt-get install -y -qq --no-install-recommends wine
+fi
+
 opm install; 
 opm install 1testrunner; 
 opm install 1bdd; 
