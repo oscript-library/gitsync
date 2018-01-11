15f4dd4746f547d3396d152a88aeb81176f371c4
khorevaa <khorevaa@gmail.com>
2018-01-11 16:38:23 +0300

    Небольшая оптимизация

--- travis-ci.sh
+++ travis-ci.sh
@@ -7,19 +7,25 @@ git config --global user.name $GITNAME
 git config --global user.email $GITEMAIL
 
 
+wget -O os.deb http://oscript.io/downloads/1_0_19/onescript-engine_1.0.19_all.deb
+sudo dpkg -i *.deb; sudo apt install -f
 
+rm os.deb
 # if [ "$TRAVIS_OS_NAME" = "linux" ]; then 
     # if [ ! test $(wine --version) ]; then
 
     echo "Устанавливаю Wine"
-    add-apt-repository ppa:ubuntu-wine/ppa
+    # add-apt-repository ppa:ubuntu-wine/ppa
     apt-get update
-    apt-get install wine1.8 winetricks
+    apt-get install -ywine winetricks
    
     # fi
 # fi
 
-opm install; 
+
 opm install 1testrunner; 
 opm install 1bdd; 
+
+opm install; 
+
 opm test; 
