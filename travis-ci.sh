d9ac7aa9a6027888745785643a2927c16ad1ceb3
Aleksey Khorev <665525+khorevaa@users.noreply.github.com>
2018-05-24 21:06:09 +0300

    Update travis-ci.sh

--- travis-ci.sh
+++ travis-ci.sh
@@ -12,17 +12,20 @@ echo "Устанавливаю версию OScript <$OSCRIPT_VERSION>"
 curl http://oscript.io/downloads/$OSCRIPT_VERSION/deb > oscript.deb 
 dpkg -i oscript.deb 
 rm -f oscript.deb
-
-echo "Установка зависимостей"
+echo "==================================="
+echo "Установка зависимостей тестирования"
 opm install 1testrunner; 
 opm install 1bdd;
 opm install coverage;
 opm update opm 
 
 opm install; 
+echo "==================================="
+echo "Установка opm из develop"
 opm run install-opm-global;
 
 cat /usr/bin/opm
+echo "==================================="
 
 echo "Запуск тестирования пакета"
 oscript ./tasks/coverage.os; 
